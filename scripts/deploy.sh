bd9b68fde30565ab99bb0c8c886813ff6a47630c
n00best <marcano.daniel98@gmail.com>
2021-08-16 17:51:49 -0500

    created the deploy.sh file for deploying all yamls into a single one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && printf "%q\n" "$(pwd)" )"
 
-pushd "$SCRIPT_DIR"
+pushd "$SCRIPT_DIR" &> /dev/null
     cd .. 
     readonly PARENT_DIR="$( printf "%q\n" "$(pwd)" )"
-popd
+popd &> /dev/null
 
 readonly COMPILE_SCRIPT="$SCRIPT_DIR/compile.sh"
+
+declare -a SYSTEM_FILES
+SYSTEM_FILES=(namespace)
 
 declare -a DATABASE_FILES
 DATABASE_FILES=(secret configmap volume persistentvolumeclaim deployment service)
@@ -19,39 +22,34 @@ API_FILES=(deployment service)
 
 declare -a filesToMerge 
 
-echo "SCRIPT DIR: $SCRIPT_DIR"
-echo "PARENT DIR: $PARENT_DIR"
-exit 0
+readonly FILES_SOURCE_DIR="$PARENT_DIR/source"
 
-$(
-    pushd "$PARENT_DIR/source"
-        readonly FILES_SOURCE_DIR="$( printf "%q\n" "$(pwd)" )"
-    popd
-)
 
 i=0
 
 # Functions 
 
 addFile() {
-    echo "$1/$2.yaml"
+    echo "$1/$2-$3.yaml"
 }
 
 # main
+for file in "${SYSTEM_FILES[@]}"; do
+    filesToMerge[i]=$(addFile $FILES_SOURCE_DIR "system" $file)
+    ((i+=1))
+done
 
 for file in "${DATABASE_FILES[@]}"; do
-    filesToMerge[i]=$(addFile $FILES_SOURCE_DIR $file)
+    filesToMerge[i]=$(addFile $FILES_SOURCE_DIR "database" $file)
     ((i+=1))
 done
 
 for file in "${API_FILES[@]}"; do
-    filesToMerge[i]=$(addFile $FILES_SOURCE_DIR $file)
+    filesToMerge[i]=$(addFile $FILES_SOURCE_DIR "api" $file)
     ((i+=1))
 done
 
-for file in "${filesToMerge[@]}"; do
-    echo $file
-done
-
-exit 0
 $( $COMPILE_SCRIPT ${filesToMerge[@]} )
+
+echo "Deployment finished properly"
+exit 0
